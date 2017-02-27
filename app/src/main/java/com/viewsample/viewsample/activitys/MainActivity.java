1671a868c1380c124f961bdd927967de880221a2
ImmprtalHalfWu <1426488898@qq.com>
2017-02-27 09:24:56 +0800

    准备写FadingTextView

--- app/src/main/java/com/viewsample/viewsample/activitys/MainActivity.java
+++ app/src/main/java/com/viewsample/viewsample/activitys/MainActivity.java
@@ -18,36 +18,36 @@ import com.viewsample.viewsample.R;
 public class MainActivity extends SuperActivity {
 
     private ListView mListview;
-    private List<ListviewItemDate> listDate;
+    private List<ListviewItemData> listData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initDate();
+        initData();
         initView();
 
     }
 
-    private void initDate() {
-        listDate = new ArrayList<>();
-        listDate.add(new ListviewItemDate("钟表", com.viewsample.viewsample.activitys.ActivityClock.class));
-        listDate.add(new ListviewItemDate("图文", com.viewsample.viewsample.activitys.ActivityImageWithTxt.class));
-        listDate.add(new ListviewItemDate("简单进度条", com.viewsample.viewsample.activitys.ActivityProgress.class));
-        listDate.add(new ListviewItemDate("音量控制",com.viewsample.viewsample.activitys. ActivitySound.class));
-        listDate.add(new ListviewItemDate("手势锁", com.viewsample.viewsample.activitys.ActivityLock.class));
-        listDate.add(new ListviewItemDate("侧滑", com.viewsample.viewsample.activitys.ActivitySideSlip.class));
-        listDate.add(new ListviewItemDate("线性布局分割线", com.viewsample.viewsample.activitys.ActivityLinear.class));
-//        listDate.add(new ListviewItemDate("表单布局", com.viewsample.viewsample.activitys.ActivityFromList.class));
-        listDate.add(new ListviewItemDate("课程表", com.viewsample.viewsample.activitys.ActivityTimeTable.class));
-        listDate.add(new ListviewItemDate("动态添加spinner", com.viewsample.viewsample.activitys.ActivityListViewSpinner.class));
-        listDate.add(new ListviewItemDate("上拉加载", com.viewsample.viewsample.activitys.ActivityLoadMore.class));
-        listDate.add(new ListviewItemDate("微信视频进度条按钮", com.viewsample.viewsample.activitys.ActivityProgressButton.class));
-        listDate.add(new ListviewItemDate("微信视频伸缩布局", com.viewsample.viewsample.activitys.ActivityStretchOutLayout.class));
-        listDate.add(new ListviewItemDate("微信小视频", com.viewsample.viewsample.activitys.ActivityWeChatSmallVideo.class));
-        listDate.add(new ListviewItemDate("X5内核WebView", ActivityTencentX5WebView.class));
-        listDate.add(new ListviewItemDate("渐入渐出TextView", ActivityFadingTextView.class));
+    private void initData() {
+        listData = new ArrayList<>();
+        listData.add(new ListviewItemData("钟表", com.viewsample.viewsample.activitys.ActivityClock.class));
+        listData.add(new ListviewItemData("图文", com.viewsample.viewsample.activitys.ActivityImageWithTxt.class));
+        listData.add(new ListviewItemData("简单进度条", com.viewsample.viewsample.activitys.ActivityProgress.class));
+        listData.add(new ListviewItemData("音量控制",com.viewsample.viewsample.activitys. ActivitySound.class));
+        listData.add(new ListviewItemData("手势锁", com.viewsample.viewsample.activitys.ActivityLock.class));
+        listData.add(new ListviewItemData("侧滑", com.viewsample.viewsample.activitys.ActivitySideSlip.class));
+        listData.add(new ListviewItemData("线性布局分割线", com.viewsample.viewsample.activitys.ActivityLinear.class));
+//        listData.add(new ListviewItemData("表单布局", com.viewsample.viewsample.activitys.ActivityFromList.class));
+        listData.add(new ListviewItemData("课程表", com.viewsample.viewsample.activitys.ActivityTimeTable.class));
+        listData.add(new ListviewItemData("动态添加spinner", com.viewsample.viewsample.activitys.ActivityListViewSpinner.class));
+        listData.add(new ListviewItemData("上拉加载", com.viewsample.viewsample.activitys.ActivityLoadMore.class));
+        listData.add(new ListviewItemData("微信视频进度条按钮", com.viewsample.viewsample.activitys.ActivityProgressButton.class));
+        listData.add(new ListviewItemData("微信视频伸缩布局", com.viewsample.viewsample.activitys.ActivityStretchOutLayout.class));
+        listData.add(new ListviewItemData("微信小视频", com.viewsample.viewsample.activitys.ActivityWeChatSmallVideo.class));
+        listData.add(new ListviewItemData("X5内核WebView", ActivityTencentX5WebView.class));
+        listData.add(new ListviewItemData("渐入渐出TextView", ActivityFadingTextView.class));
 
     }
 
@@ -60,7 +60,7 @@ public class MainActivity extends SuperActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 // TODO Auto-generated method stub
-                startActivity(new Intent(MainActivity.this, listDate.get((int) id).nextActivity));
+                startActivity(new Intent(MainActivity.this, listData.get((int) id).nextActivity));
             }
         });
     }
@@ -70,13 +70,13 @@ public class MainActivity extends SuperActivity {
         @Override
         public int getCount() {
             // TODO Auto-generated method stub
-            return listDate == null ? 0 : listDate.size();
+            return listData == null ? 0 : listData.size();
         }
 
         @Override
         public Object getItem(int position) {
             // TODO Auto-generated method stub
-            return listDate.get(position);
+            return listData.get(position);
         }
 
         @Override
@@ -92,7 +92,7 @@ public class MainActivity extends SuperActivity {
                 convertView = MainActivity.this.getLayoutInflater().inflate(R.layout.item_main_listview, parent, false);
             }
 
-            ((TextView)convertView).setText(listDate.get(position).itemName);
+            ((TextView)convertView).setText(listData.get(position).itemName);
 
             return convertView;
         }
@@ -100,13 +100,13 @@ public class MainActivity extends SuperActivity {
 
     }
 
-    final class ListviewItemDate{
+    final class ListviewItemData{
 
         public final String itemName;
         public final Class<?> nextActivity;
 
 
-        public ListviewItemDate(String name, Class<?> activity) {
+        public ListviewItemData(String name, Class<?> activity) {
             itemName = name;
             nextActivity = activity;
         }
